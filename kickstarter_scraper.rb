9688aa162fe05ad6d31b694e2659f4746ccae77d
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-11 01:53:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'pry'
 
 # projects: kickstarter.css("li.project.gird_4")
+# title: project.css("h2.bbcard_name strong a").text
 
 def create_project_hash
   # write your code here
