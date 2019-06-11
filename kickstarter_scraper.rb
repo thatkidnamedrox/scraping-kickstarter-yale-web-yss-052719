da5f7d4a782d0bd15b32c03852dd773c8a3eb8fa
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-11 01:52:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,8 @@
 require 'nokogiri'
 require 'pry'
 
+# projects: kickstarter.css("li.project.gird_4")
+
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
